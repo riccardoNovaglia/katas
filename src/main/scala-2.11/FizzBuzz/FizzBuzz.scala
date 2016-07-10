ed4a356748645499d563c0db93272b5eb53b5c21
riccardoNovaglia <riccardo.novaglia@gmail.com>
2016-07-10 10:00:56 +0100

    FizzBuzz: another interesting solution. Cool solution but complex implementation?

--- src/main/scala-2.11/FizzBuzz/FizzBuzz.scala
+++ src/main/scala-2.11/FizzBuzz/FizzBuzz.scala
@@ -5,15 +5,29 @@ class FizzBuzz {
   implicit def intToDivisible(int: Int): IsDivisible = new IsDivisible(int)
 
   def apply(value: Int): String = {
-    (value isDivisibleBy 3, value isDivisibleBy 5) match {
-      case (true, true) => "fizzbuzz"
-      case (true, _) => "fizz"
-      case (_, true) => "buzz"
-      case _ => value.toString
-    }
+    value report "fizz" when isDivisibleBy(3) report "buzz" when isDivisibleBy(5) orToStringIfNeither
+  }
+
+  def isDivisibleBy(divisor: Int): (Int) => Boolean = {
+    value => value % divisor == 0
   }
 }
 
 case class IsDivisible(value: Int) {
-  def isDivisibleBy(divisor: Int) = value % divisor == 0
+  var report: String = _
+  var result: String = ""
+
+  def report(toReport: String): IsDivisible = {
+    report = toReport
+    this
+  }
+
+  def when(test: (Int) => Boolean): IsDivisible = {
+    if (test(value)) {
+      result += report
+    }
+    this
+  }
+
+  def orToStringIfNeither: String = if (result.isEmpty) value.toString else result
 }
